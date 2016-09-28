8a314c3d41dd038ac6ca98473dc8d0e11e363511
Ben <benjaminlagrone@gmail.com>
2016-09-28 15:56:52 -0500

    Cleanup commit

---
+++ app/app.js
@@ -0,0 +1,25 @@
+console.log("How long to get to work");
+
+function reqListener () {
+  console.log(this.responseText);
+  var respondArea = document.getElementById("response");
+  //respondArea.innerHTML = this.responseText.data;
+  respondArea.innerHTML = this.responseText;
+}
+
+
+var apiKey = 'd33a54a2-9971-4119-8286-42f5566d8bea';
+var point1 = '29.66885,-95.4563095';
+var point2 = '29.7008786,-95.3985539';
+var vehicle = 'bike';
+var dataType = 'json'
+var calc_points = false;
+var instructions = false;
+var debug = true;
+//var url = 'https://graphhopper.com/api/1/route?point=' + point1 + '&point=' + point2 + '&vehicle=' + vehicle + '&debug=' + debug + '&key=' + apiKey + '&type=' + dataType + '&calc_points=' + calc_points + '&instructions=' + instructions;
+//var url = 'https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyA81MJzCGhIfnialvX9hlbBJawdEtKBo4k'
+var url = "/app/data.txt";
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", url);
+oReq.send();
