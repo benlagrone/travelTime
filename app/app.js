2dcd2beb77c946dcbb2d5c9cfb28578d7e528838
Ben <benjaminlagrone@gmail.com>
2016-09-28 16:07:56 -0500

    Cleanup commit 2

--- app/app.js
+++ app/app.js
@@ -8,16 +8,15 @@ function reqListener () {
 }
 
 
-var apiKey = 'd33a54a2-9971-4119-8286-42f5566d8bea';
-var point1 = '29.66885,-95.4563095';
-var point2 = '29.7008786,-95.3985539';
-var vehicle = 'bike';
-var dataType = 'json'
-var calc_points = false;
-var instructions = false;
-var debug = true;
+//var apiKey = 'd33a54a2-9971-4119-8286-42f5566d8bea';
+//var point1 = '29.66885,-95.4563095';
+//var point2 = '29.7008786,-95.3985539';
+//var vehicle = 'bike';
+//var dataType = 'json'
+//var calc_points = false;
+//var instructions = false;
+//var debug = true;
 //var url = 'https://graphhopper.com/api/1/route?point=' + point1 + '&point=' + point2 + '&vehicle=' + vehicle + '&debug=' + debug + '&key=' + apiKey + '&type=' + dataType + '&calc_points=' + calc_points + '&instructions=' + instructions;
-//var url = 'https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyA81MJzCGhIfnialvX9hlbBJawdEtKBo4k'
 var url = "/app/data.txt";
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
